8e0054a1070ebc1620daf92a99eb869f671bb405
Rutuja <rutujakhairnar212@gmail.com>
2026-06-23 20:38:29 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT ?? 8080
+
+app.get("/", (req, res) => {
+    res.json({ msg: "Hello World" })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up running on PORT ${PORT}`)
+})
